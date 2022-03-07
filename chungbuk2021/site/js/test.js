704fd79ac5392bde8ffe7ddc45b7b193369c7165
gunami <58508367+gunami4573@users.noreply.github.com>
2022-03-07 16:29:16 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	README.md
    #	chungbuk2021/repository/1.html
    #	chungbuk2021/test.html

--- chungbuk2021/site/js/test.js
+++ chungbuk2021/site/js/test.js
@@ -13,7 +13,6 @@
         //여기서부터 코드 작성해주세요
         $('.main').on('click', function(){
            var $this = $(this);
-
            console.log('GitHub main page test console error');
         });
 
