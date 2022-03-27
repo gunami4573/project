543e7e549c33650fe41c0dbd6d74c7025dbe1ddb
gunami <bigunamict@naver.com>
2022-03-27 15:50:11 +0900

    2022 의정부시청 미래혁신교육협력센터 신규(작업중)

--- 2022ujb/site/miracleolle/js/main.js
+++ 2022ujb/site/miracleolle/js/main.js
@@ -12,6 +12,28 @@
 
         //여기서부터 코드 작성해주세요
 
+        //메인비주얼 버튼 슬라이드 시작
+        var $LinkSldieList = $('.miracle_visual .miracle_visual_wrap .link_slide_wrap .link_slide_list');
+        $LinkSldieList.slick({
+            autoplay : false,
+            arrows : true,
+            dots : false,
+            slidesToShow : 2,
+            slidesToScroll : 1,
+            infinite : false,
+            swipe : true,
+            swipeToSlide : true,
+            draggable : true,
+            rows : 1,
+            responsive: [{
+                breakpoint : 641,
+                settings : {
+                    slidesToShow : 1
+                }
+            }]
+        });
+        //메인비주얼 버튼 슬라이드 끝
+
 
         $window.on('screen:tablet screen:phone', function(event) {
 
