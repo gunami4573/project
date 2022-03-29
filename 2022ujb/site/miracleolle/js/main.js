1ba29818d1306370a938a87aea2a6d35ab32b94c
gunami <58508367+gunami4573@users.noreply.github.com>
2022-03-29 17:40:55 +0900

    2022 의정부시청 미래혁신교육협력센터 신규(작업중)

--- 2022ujb/site/miracleolle/js/main.js
+++ 2022ujb/site/miracleolle/js/main.js
@@ -13,14 +13,34 @@
         //여기서부터 코드 작성해주세요
 
         //메인비주얼 버튼 슬라이드 시작
-        var $LinkSldieList = $('.miracle_visual .miracle_visual_wrap .link_slide_wrap .link_slide_list');
+        var $LinkSldieList = $('.miracle_visual .miracle_visual_wrap .link_slide_wrap .link_slide_list'),
+            LinkSldieTotal = $('.miracle_visual .miracle_visual_wrap .link_slide_control .total'),
+            LinkSldieCurrent = $('.miracle_visual .miracle_visual_wrap .link_slide_control .current');
         $LinkSldieList.slick({
             autoplay : false,
             arrows : true,
+            prevArrow : $('.miracle_visual .miracle_visual_wrap .link_slide_control .prev'),
+            nextArrow : $('.miracle_visual .miracle_visual_wrap .link_slide_control .next'),
+            autoArrow : $('.miracle_visual .miracle_visual_wrap .link_slide_control .auto'),
+            pauseText : '정지',
+            playText : '재생',
+            total : LinkSldieTotal,
+            current : LinkSldieCurrent,
+            customState : function(state) {
+                //현재 슬라이드 위치가 10보다 작을 때
+                if(state.current < 10) {
+                    state.current = state.current;
+                }
+                //슬라이드 갯수가 10보다 작을 때
+                if(state.total < 10) {
+                    state.total = state.total;
+                }
+                return state;
+            },
             dots : false,
             slidesToShow : 2,
             slidesToScroll : 1,
-            infinite : false,
+            infinite : true,
             swipe : true,
             swipeToSlide : true,
             draggable : true,
@@ -123,7 +143,12 @@
             variableWidth : false,
             prevArrow : $('.photo_slide_wrap .photo_slide_control .prev'),
             nextArrow : $('.photo_slide_wrap .photo_slide_control .next'),
-            responsive: [{}]
+            responsive: [{
+                breakpoint : 1401,
+                settings : {
+                    slidesToShow : 3
+                }
+            }]
         });
         //포로갤러리 슬라이드 끝
 
