4bdd0c802ddf01d2c279962b0ca4ffe5fcc43842
gunami <58508367+gunami4573@users.noreply.github.com>
2022-06-27 16:37:58 +0900

    2022 제61회 옥천군 충북도민체육대회 신규

--- 2022ocsports/site/sports/js/sub.js
+++ 2022ocsports/site/sports/js/sub.js
@@ -100,22 +100,21 @@
         //컨텐츠 탭메뉴 끝
 
         //반응형 테이블 시작
-        $('table.table.responsive').not($('.prettyprint').children()).each(function () {
+        $('table.responsive').not($('.prettyprint').children()).each(function() {
             var RowSpanExist = $(this).find('td, th').is('[rowspan]'),
                 TheadExist = $(this).find('thead').length;
-            if ((RowSpanExist == false) && (TheadExist != 0)) {//rowspan이 없을 경우만 실행 (rowspan이 있으면 지원불가)
-                $(this).children('tbody').children('tr').find('th, td').each(function () {
+            if((RowSpanExist==false) && (TheadExist!=0)){//rowspan이 없을 경우만 실행 (rowspan이 있으면 지원불가)
+                $(this).children('tbody').children('tr').find('th, td').each(function() {
                     var ThisIndex = $(this).index(),
                         TheadText = $(this).parents('tbody').siblings('thead').find('th').eq(ThisIndex).text();
                     $(this).attr('data-content', TheadText);
                 });
-                $(this).children('tfoot').children('tr').find('th, td').each(function () {
+                $(this).children('tfoot').children('tr').find('th, td').each(function() {
                     var ThisIndex = $(this).index(),
                         TheadText = $(this).parents('tfoot').siblings('thead').find('th').eq(ThisIndex).text();
                     $(this).attr('data-content', TheadText);
                 });
-            }
-            ;
+            };
         });
         //반응형 테이블 끝
 
