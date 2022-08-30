565f920223d9ca181828d3007ae9ff74a9dcdf59
gunami <58508367+gunami4573@users.noreply.github.com>
2022-08-30 17:09:48 +0900

    2022 미동산수목원 신규

--- 2022midongsan/site/midongsan/js/main.js
+++ 2022midongsan/site/midongsan/js/main.js
@@ -1,5 +1,15 @@
 (function ($) {
     'use strict';
+    function splittingTextDelay (object, speed, delay_speed) {
+        var splitLength = $(object).find('.char').length;
+        for (var i=0; i<splitLength; i++) {
+            if (  $(object).data('css-property') == 'animation' ) {
+                $(object).find('.char').eq(i).css('animation-delay',delay_speed+(i*speed)+'s');
+            }else if( $(object).data('css-property') == 'transition' ) {
+                $(object).find('.char').eq(i).css('transition-delay',delay_speed+(i*speed)+'s');
+            }
+        }
+    }
 
     var $window = $(window),
         $document = $(document),
@@ -11,6 +21,41 @@
 
         //여기서부터 코드 작성해주세요
 
+        //텍스트 에니메이션 플러그인 시작
+        Splitting({
+            target: '[data-splitting]',
+            by: 'chars',
+            key: null
+        });
+        var $splittingTxt = $('.word-split');
+        $($splittingTxt).each(function  () {
+            splittingTextDelay($(this),$(this).data('speed'),$(this).data('speed-delay'));
+        });
+        //텍스트 에니메이션 플러그인 끝
+
+        //메인 비주얼 슬라이드 시작
+        var $MidongSlideList = $('.main_visual .main_visual_wrap .visual_inner_box .wrap_box .midong_slide_wrap .midong_slide_list'),
+            $MidongSlidePrev = $('.main_visual .main_visual_wrap .visual_inner_box .wrap_box .midong_slide_wrap .midong_slide_control .prev'),
+            $MidongSlideNext = $('.main_visual .main_visual_wrap .visual_inner_box .wrap_box .midong_slide_wrap .midong_slide_control .next');
+        $MidongSlideList.slick({
+            autoplay : false,
+            autoplaySpeed : 2000,
+            speed : 1200,
+            dots : false,
+            arrows : true,
+            prevArrow : $MidongSlidePrev,
+            nextArrow : $MidongSlideNext,
+            slidesToShow : 1,
+            slidesToScroll : 1,
+            infinite : true,
+            swipe : false,
+            swipeToSlide : false,
+            draggable : false,
+            zIndex : 1,
+            responsive: [{}]
+        });
+
+        //메인 비주얼 슬라이드 끝
 
         $window.on('screen:tablet screen:phone', function (event) {
 
