829ebc442d96e402a43e05f55a95e2f22f986691
gunami <58508367+gunami4573@users.noreply.github.com>
2022-08-31 17:32:13 +0900

    2022 미동산수목원 신규

--- 2022midongsan/site/midongsan/js/main.js
+++ 2022midongsan/site/midongsan/js/main.js
@@ -38,7 +38,7 @@
             $MidongSlidePrev = $('.main_visual .main_visual_wrap .visual_inner_box .wrap_box .midong_slide_wrap .midong_slide_control .prev'),
             $MidongSlideNext = $('.main_visual .main_visual_wrap .visual_inner_box .wrap_box .midong_slide_wrap .midong_slide_control .next');
         $MidongSlideList.slick({
-            autoplay : true,
+            autoplay : false,
             autoplaySpeed : 4000,
             speed : 1200,
             dots : false,
@@ -55,8 +55,57 @@
             pauseOnHover : false,
             responsive: [{}]
         });
-
         //메인 비주얼 슬라이드 끝
+
+        //프로그램 슬라이드 및 탭 시작
+        var $TabSlideItem = $('.service .service_wrap .service_tab .tab_slide_box .tab_slide_item');
+        $TabSlideItem.each(function(){
+            var $this = $(this),
+                $ServiceSlideWrap = $this.find('.service_slide_wrap'),
+                $ServiceSlideList = $ServiceSlideWrap.find('.service_slide_list'),
+                $ServiceSlideControl = $ServiceSlideList.siblings('.service_slide_control');
+            $ServiceSlideList.slick({
+                autoplay : false,
+                dots : false,
+                arrows : true,
+                prevArrow : $ServiceSlideControl.find('.prev'),
+                nextArrow : $ServiceSlideControl.find('.next'),
+                slidesToShow : 3,
+                slidesToScroll : 1,
+                infinite : true,
+                swipe : true,
+                swipeToSlide : true,
+                draggable : true,
+                zIndex : 1,
+                pauseOnHover : false,
+                responsive: [{}]
+            });
+        });
+        $('.service .service_wrap .service_tab .tab_box .tab_box_list .tab_box_item button.tab_box_btn').on('click', function(){
+            var $this = $(this),
+                $MyTabBoxItem = $this.parent('.tab_box_item'),
+                IsActive = $MyTabBoxItem.is('.active'),
+                MyTabBoxIndex = $MyTabBoxItem.index(),
+                $OtherTabBoxItem = $MyTabBoxItem.siblings('.tab_box_item'),
+                $OtherTabBoxBtn = $OtherTabBoxItem.find('button.tab_box_btn'),
+                $MyTabBoxList = $MyTabBoxItem.parent('.tab_box_list'),
+                $TabBox = $MyTabBoxList.parent('.tab_box'),
+                $TabSlideBox = $TabBox.siblings('.tab_slide_box'),
+                $MyTabSlideItem = $TabSlideBox.find('.tab_slide_item').eq(MyTabBoxIndex),
+                $OtherTabSlideItem = $MyTabSlideItem.siblings('.tab_slide_item'),
+                $MySlideWrap = $MyTabSlideItem.find('.service_slide_wrap'),
+                $MySlideList = $MySlideWrap.find('.service_slide_list');
+            if(!IsActive){
+                $OtherTabBoxItem.removeClass('active');
+                $OtherTabBoxBtn.removeAttr('title');
+                $MyTabBoxItem.addClass('active');
+                $this.attr('title', '선택됨');
+                $OtherTabSlideItem.removeClass('active');
+                $MyTabSlideItem.addClass('active');
+                $MySlideList.slick('setPosition');
+            }
+        });
+        //프로그램 슬라이드 및 탭 끝
 
         $window.on('screen:tablet screen:phone', function (event) {
 
