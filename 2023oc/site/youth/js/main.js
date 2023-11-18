d07e894816d436985e7ddefb3968c710e1fe1bd6
gunami4573 <bigunamict@naver.com>
2023-11-18 11:20:02 +0900

    2023 옥천군청 전면개편

---
+++ 2023oc/site/youth/js/main.js
@@ -0,0 +1,159 @@
+(function ($) {
+    //비주얼 svg 프로그레스 시작
+    var myAnim;
+    var myTextAnim;
+    var Nowprogress = 0;
+    function Loading() {
+        Nowprogress = 0;
+        var count = $('#count');
+        myTextAnim = $({Counter: 0}).animate({Counter: 100}, {
+            duration: 5000,
+            easing: 'linear',
+            step: function () {
+                count.text(Math.ceil(this.Counter) + "%");
+                Nowprogress = Math.ceil(this.Counter);
+            }
+        });
+        var s = Snap('#animated');
+        var progress = s.select('#progress');
+        progress.attr({strokeDasharray: '0, 251.2'});
+        myAnim = Snap.animate(0, 251.2, function (value) {
+            progress.attr({'stroke-dasharray': value + ',251.2'});
+        }, 5000);
+    }
+    function Loadingpause() {
+        myAnim.pause();
+        myTextAnim.stop(true, false);
+        Nowprogress = myTextAnim[0].Counter;
+        $('#count').text(myTextAnim[0].Counter + '%');
+    }
+    function Loadingresume() {
+        var Nowduration = 30 * (100 - Nowprogress);
+        var count = $('#count');
+        myTextAnim = $({Counter: Nowprogress}).animate({Counter: 100}, {
+            duration: Nowduration,
+            easing: 'linear',
+            step: function () {
+                count.text(Math.ceil(this.Counter) + "%");
+            }
+        });
+        var s = Snap('#animated');
+        var progress = s.select('#progress');
+        var NowDegree = (251.2 / 100) * Nowprogress;
+        progress.attr({strokeDasharray: NowDegree + ', 251.2'});
+        myAnim = Snap.animate(NowDegree, 251.2, function (value) {
+            progress.attr({'stroke-dasharray': value + ',251.2'});
+        }, Nowduration, function () {
+
+        });
+    }
+    //비주얼 svg 프로그레스 끝
+    'use strict';
+
+    var $window = $(window),
+        $document = $(document),
+        $html = $('html'),
+        $head = $('head'),
+        $screen = $.screen,
+        $inArray = $.inArray;
+    $(function () {
+
+        //여기서부터 코드 작성해주세요
+
+        //퀵메뉴 시작
+        function quickScroll() {
+            if($window.scrollTop() > 161) {
+                $('.youth_quick').removeClass('active');
+                $('.youth_quick .youth_quick_inner button.youth_quick_btn').attr('title', '퀵메뉴 열기');
+            }
+            if($window.scrollTop() <= 161) {
+                $('.youth_quick').addClass('active');
+                $('.youth_quick .youth_quick_inner button.youth_quick_btn').attr('title', '퀵메뉴 닫기');
+            }
+        }
+        $window.scroll(function(){
+            quickScroll();
+        });
+        quickScroll();
+
+        $('.youth_quick .youth_quick_inner button.youth_quick_btn').on('click', function(){
+            var $this = $(this),
+                $YouthQuick = $this.parents('.youth_quick'),
+                IsActive = $YouthQuick.is('.active');
+            if(!IsActive){
+                $this.attr('title', '퀵메뉴 닫기');
+                $YouthQuick.addClass('active');
+            }
+            else{
+                $this.attr('title', '퀵메뉴 열기');
+                $YouthQuick.removeClass('active');
+            }
+        });
+        //퀵메뉴 끝
+
+        //비주얼 영역 슬라이드 시작
+        var $VisualSlideList = $('.visual_wrap .visual_slide_wrap .visual_slide_list'),
+            $VisualSlidePrev = $('.visual_wrap .visual_slide_wrap .visual_slide_control .prev'),
+            $VisualSlideNext = $('.visual_wrap .visual_slide_wrap .visual_slide_control .next'),
+            $VisualSlideAuto = $('.visual_wrap .visual_slide_wrap .visual_slide_control .auto'),
+            $VisualSlideCurrent = $('.visual_wrap .visual_slide_wrap .visual_slide_control .current'),
+            $VisualSlideTotal = $('.visual_wrap .visual_slide_wrap .visual_slide_control .total');
+        $VisualSlideList.on('init', function(event, slick, currentSlide, nextSlide) {
+            Loading();
+        });
+        $VisualSlideList.slick({
+            //기본
+            autoplay : true,
+            autoplaySpeed : 5000,
+            speed : 2000,
+            dots : false,
+            draggable : true,
+            swipe : true,
+            swipeToSlide : true,
+            slidesToShow : 1,
+            slidesToScroll : 1,
+            variableWidth : false,
+            infinite : true,
+            arrows : true,
+            prevArrow : $VisualSlidePrev,
+            nextArrow : $VisualSlideNext,
+            autoArrow : $VisualSlideAuto,
+            pauseText : '정지',
+            playText : '재생',
+            total : $VisualSlideTotal,
+            current : $VisualSlideCurrent,
+            pauseOnArrowClick : true,
+            pauseOnHover : false,
+            pauseOnSwipe : false,
+            pauseOnDotsClick : false,
+            zIndex : 1,
+            fade : true,
+            responsive : [{}]
+        });
+        $VisualSlideList.on('beforeChange', function(event, slick, currentSlide) {
+            var IsAutoplay = $VisualSlideAuto.is('.slick-pause');
+            if(IsAutoplay){
+                Loading();
+            }
+        });
+        $VisualSlideAuto.on('click', function(){
+            var $this = $(this),
+                IsPlaying = $this.is('.slick-play');
+            if(IsPlaying){
+                Loadingpause();
+            }else{
+                Loadingresume();
+            }
+        });
+        $VisualSlideList.on({
+            mousedown: function(){
+                var IsPlaying = $VisualSlideAuto.is('.slick-pause');
+                if(IsPlaying){
+                    $VisualSlideAuto.click();
+                }
+            }
+        });
+        //비주얼 영역 슬라이드 끝
+
+    });
+})(jQuery);
