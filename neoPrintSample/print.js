48fcd325e06cb323432240a6181cae77d259d3f5
gunami4573 <bigunamict@naver.com>
2024-02-07 17:07:44 +0900

    Neo(HS) Print 교육용 // https://belugacurtain.github.io/project/neoPrintSample

--- neoPrintSample/print.js
+++ neoPrintSample/print.js
@@ -11,42 +11,51 @@
             var $this = $(this);
             $window.on('scroll', function () {
                 if ($window.scrollTop() >= $this.offset().top) {
-                    setTimeout(function(){
+                    setTimeout(function () {
                         $this.addClass('blr');
                     }, 1);
                 } else {
-                    setTimeout(function(){
+                    setTimeout(function () {
                         $this.removeClass('blr');
                     }, 1);
                 }
             });
         });
 
+        // 새창 브라우저를 담을 빈 배열 선언
+        var printWindowArray = [];
+
         var $PrintBtn = $('button.print_btn');
         $PrintBtn.each(function () {
             var $PrintDesc = $(this).siblings('.print_desc');
             $(this).on('mouseenter', function () {
-                setTimeout(function(){
+                setTimeout(function () {
                     $PrintDesc.addClass('blr');
                 }, 1);
             }).on('mouseleave', function () {
-                setTimeout(function(){
+                setTimeout(function () {
                     $PrintDesc.removeClass('blr');
                 }, 1);
             }).on('click', function () {
-                
-                // 새창 브라우저 너비 값 지정
-                var printWindowWidth = 1000;
-                // 새창 브라우저 높이 값 지정
-                var printWindowHeight = 700;
 
-                // 새창 브라우저 가운데 위치 지정
+                // 새창 브라우저 너비 , 높이 ,가운데 위치 값 선언
                 // ( ★주의★ 모니터 두개 이상 사용시 메인 모니터 에서만 가운데 정렬 됨 )
+                var printWindowWidth = 1000;
+                var printWindowHeight = 700;
                 var printWindowTop = (window.screen.height / 2) - (printWindowHeight / 2);
                 var printWindowLeft = (window.screen.width / 2) - (printWindowWidth / 2);
 
-                // 새창으로 띄울 브라우저 변수에 담기
-                var printWindow = window.open("/", "_blank", 'width='+printWindowWidth+', height='+printWindowHeight+', top='+printWindowTop+', left='+printWindowLeft+'');
+                // 새창으로 띄울 브라우저 변수에 담은 후 너비 , 높이 , 가운데 위치 값 지정
+                var printWindow = window.open("/", "_blank", 'width=' + printWindowWidth + ', height=' + printWindowHeight + ', top=' + printWindowTop + ', left=' + printWindowLeft + '');
+
+                // 새창 브라우저를 담을 배열의 길이 값 측정
+                var printWindowArrayLength = printWindowArray.length;
+
+                // 새창 브라우저를 담을 배열의 길이가 측정될 때, 0번째 삭제 후 다시 빈 배열로 만들기
+                if (printWindowArrayLength) {
+                    printWindowArray[0].close();
+                    printWindowArray = [];
+                }
 
                 // css, js 복사를 위함
                 var $head = $('head').clone();
@@ -60,32 +69,31 @@
 
                 // 새창으로 띄울 브라우저 문서 doctype 작성
                 printWindow.document.write(
-                    '<!DOCTYPE html>'+
-                    '<html>'+
-                    '<head>'+headHtml+'</head>'+
-                    '<body class="new_body">' +
-                        '<div class="print_box">' +
-                            '<div class="print_desc">'
-                                +PrintDescHtml+
-                            '</div><!--// print_desc-->' +
-                        '</div><!--// print_box-->' +
-                        '<div class="loading_print">Loading....</div><!--// loading_print-->' +
-                    '</body><!--// new_body-->'+
+                    '<!DOCTYPE html>' +
+                    '<html>' +
+                        '<head>' +
+                            headHtml +
+                        '</head>' +
+                        '<body id="body" class="new_body">' +
+                            '<div class="print_box">' +
+                                '<div class="print_desc">'
+                                    + PrintDescHtml +
+                                '</div><!--// print_desc-->' +
+                            '</div><!--// print_box-->' +
+                            '<div class="loading">...loading...</div><!--//loading-->' +
+                            '<button type="button" title="Print Go!!!" onclick="window.print();window.close();" class="print_start_btn"><em>Print Start Click</em></button><!--//print_btn-->' +
+                            '<script>' +
+                                'var body = document.getElementById("body");' +
+                                'setTimeout(function(){' +
+                                    'body.classList.add("hover_use");' +
+                                '}, 2500);' +
+                            '</script>' +
+                        '</body><!--// new_body-->' +
                     '</html>'
                 );
-                setTimeout(function(){
-                    // 새창으로 띄웠던 브라우저 닫기
-                    printWindow.document.close();
-                    // 새창으로 띄운 브라우저 포커스 맞추기
-                    printWindow.focus();
-                }, 1);
-                setTimeout(function(){
-                    // 새창으로 띄운 브라우저 프린트 도구 시작
-                    printWindow.print();
-                    // 프린트 도구 닫혔을 경우 팝업 닫기
-                    printWindow.close();
-                }, 2000);
-
+                printWindowArray.push(printWindow);
+                console.log(printWindowArray);
+                setTimeout(function(){}, 1);
             });
         });
 
