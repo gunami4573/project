a9a1be1ccea806b2f2baba4cf913d2915a7f1b85
gunami4573 <bigunamict@naver.com>
2024-02-23 16:51:53 +0900

    Neo(HS) SVG 교육용 // https://belugacurtain.github.io/project/neoSlideBookSample

--- neoSlideBookSample/bookSlide.js
+++ neoSlideBookSample/bookSlide.js
@@ -16,14 +16,14 @@
             bookListPT = $bookList.css('padding-top'),
             bookListPR = $bookList.css('padding-right');
 
-        setTimeout(function(){
+        setTimeout(function () {
             //로딩 시 z-index , bottom , left 값 초기 함수 설정
             function bookItemPosition(top, right) {
-                for( var i=0; i < bookListLT; i++ ) {
+                for (var i = 0; i < bookListLT; i++) {
                     $bookItem.eq(i).css({
-                        'z-index' : bookListLT - i,
-                        'bottom' : 'calc(('+top+' / 3) * '+i+')',
-                        'left' : 'calc(('+right+' / 3) * '+i+')'
+                        'z-index': bookListLT - i,
+                        'bottom': 'calc((' + top + ' / 3) * ' + i + ')',
+                        'left': 'calc((' + right + ' / 3) * ' + i + ')'
                     });
                 }
             }
@@ -44,71 +44,81 @@
         bookItemFirstTabIndex();
 
         //Next 동작 첫 아이템 마지막으로 보냄(버튼 클릭 및 반응형 터치에 사용)
-        function bookNext(){
+        function bookNext() {
             $bookList.find('.book_item:first-child').addClass('right_move');
-            $bookItem.each(function(){
+            $bookItem.each(function () {
                 var $this = $(this),
                     thisIndex = $this.index(),
                     $PrevItem = $this.prev('.book_item'),
                     prevBottom = $PrevItem.css('bottom'),
                     prevLeft = $PrevItem.css('left');
                 $this.css({
-                    'z-index' : bookListLT - thisIndex + 1,
-                    'bottom' : 'calc('+prevBottom+')',
-                    'left' : 'calc('+prevLeft+')'
+                    'z-index': bookListLT - thisIndex + 1,
+                    'bottom': 'calc(' + prevBottom + ')',
+                    'left': 'calc(' + prevLeft + ')'
                 });
             });
             var lastBottom = $bookList.find('.book_item:last-child').css('bottom'),
                 lastLeft = $bookList.find('.book_item:last-child').css('left');
-            setTimeout(function(){
-                $bookList.append($bookList.find('.book_item:first-child').css({'z-index': 1, 'bottom' : lastBottom, 'left' : lastLeft}));
+            setTimeout(function () {
+                $bookList.append($bookList.find('.book_item:first-child').css({
+                    'z-index': 1,
+                    'bottom': lastBottom,
+                    'left': lastLeft
+                }));
                 $bookList.find('.book_item:last-child').removeClass('right_move');
 
                 bookItemFirstTabIndex();
             }, 500);
         }
-        $bookNext.on('click', function(){
+
+        $bookNext.on('click', function () {
             bookNext();
         });
 
         //Prev 동작 첫 아이템 마지막으로 보냄(버튼 클릭 및 반응형 터치에 사용)
-        function bookPrev(){
-            $bookItem.each(function(){
+        function bookPrev() {
+            $bookItem.each(function () {
                 var $this = $(this),
                     thisIndex = $this.index(),
                     $NextItem = $this.next('.book_item'),
                     nextBottom = $NextItem.css('bottom'),
                     nextLeft = $NextItem.css('left');
                 $this.css({
-                    'z-index' : bookListLT - thisIndex - 1,
-                    'bottom' : 'calc('+nextBottom+')',
-                    'left' : 'calc('+nextLeft+')'
+                    'z-index': bookListLT - thisIndex - 1,
+                    'bottom': 'calc(' + nextBottom + ')',
+                    'left': 'calc(' + nextLeft + ')'
                 });
             });
             var firstBottom = $bookList.find('.book_item:first-child').css('bottom'),
                 firstLeft = $bookList.find('.book_item:first-child').css('left');
-            $bookList.prepend($bookList.find('.book_item:last-child').css({'z-index': bookListLT, 'bottom' : firstBottom, 'left' : firstLeft}));
+            $bookList.prepend($bookList.find('.book_item:last-child').css({
+                'z-index': bookListLT,
+                'bottom': firstBottom,
+                'left': firstLeft
+            }));
             $bookList.find('.book_item:first-child').addClass('left_move');
-            setTimeout(function(){
+            setTimeout(function () {
                 $bookList.find('.book_item:first-child').removeClass('left_move');
 
                 bookItemFirstTabIndex();
             }, 500);
         }
-        $bookPrev.on('click', function(){
+
+        $bookPrev.on('click', function () {
             bookPrev();
         });
 
         //모바일에서 터치로 prev, next 동작
         var touchStart;
-        $('.book_list').bind('touchstart', function (e){
+        $('.book_list').bind('touchstart', function (e) {
             touchStart = e.originalEvent.touches[0].clientX;
         });
-        $('.book_list').bind('touchend', function (e){
+        $('.book_list').bind('touchend', function (e) {
             var touchEnd = e.originalEvent.changedTouches[0].clientX;
-            if(touchStart > touchEnd + 5){
+            if (touchStart > touchEnd + 5) {
                 bookPrev();
-            }else if(touchStart < touchEnd - 5){
+            } else if (touchStart < touchEnd - 5) {
                 bookNext();
             }
         });
@@ -118,10 +128,10 @@
             var resizeBookListPT = $('.book_list').css('padding-top');
             var resizeBookListPR = $('.book_list').css('padding-right');
 
-            for( var i=0; i < bookListLT; i++ ) {
+            for (var i = 0; i < bookListLT; i++) {
                 $('.book_list .book_item').eq(i).css({
-                    'bottom' : 'calc(('+resizeBookListPT+' / 3) * '+i+')',
-                    'left' : 'calc(('+resizeBookListPR+' / 3) * '+i+')'
+                    'bottom': 'calc((' + resizeBookListPT + ' / 3) * ' + i + ')',
+                    'left': 'calc((' + resizeBookListPR + ' / 3) * ' + i + ')'
                 });
             }
         });
