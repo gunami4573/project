d66831b161d1c12e780bb1439cf80739a4c64c38
gunami4573 <bigunamict@naver.com>
2024-02-29 17:49:46 +0900

    https://belugacurtain.github.io/project/

--- indexRoot/js/index.js
+++ indexRoot/js/index.js
@@ -4,11 +4,15 @@
 
     $(function () {
 
-        var $body = $('body'),
+        var $html = $('html'),
+            $body = $('body'),
             $wrapper = $('#wrapper'),
             $header = $('#header'),
             $footer = $('#footer');
 
+        setTimeout(function(){
+            $html.addClass('scroll_on');
+        }, 6000);
         setTimeout(function(){
             $body.addClass('loading');
         }, 1);
