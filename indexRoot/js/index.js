a5b9c18d116736112048c1f1a78a278791a067cc
gunami4573 <bigunamict@naver.com>
2024-03-06 17:39:03 +0900

    https://belugacurtain.github.io/project/

--- indexRoot/js/index.js
+++ indexRoot/js/index.js
@@ -13,22 +13,24 @@
         //
         setTimeout(function () {
             $body.addClass('loading');
-        }, 1);
+        }, 500);
         //
 
         //
         setTimeout(function () {
             $html.addClass('scroll_on');
-        }, 10000);
+        }, 10300);
         //
 
         //
         var $campanySVG = $('.campany .campany_wrap .campany_box .campany_svg_box .campany_svg'),
             $campanyG = $campanySVG.find('> g');
-        console.dir($campanyG);
+        for(var i=0; i<$campanyG.length; i++){
+            console.log($campanyG.eq(i));
+        }
         setTimeout(function () {
             $campanySVG.addClass('active');
-        }, 4000);
+        }, 4300);
         //
 
     });
