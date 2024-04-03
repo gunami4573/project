682f2b450d7f9de807971b20f78e32f29983853e
gunami4573 <bigunamict@naver.com>
2024-04-03 17:16:42 +0900

    2024 한국교원대학교 신규

--- 2024knue/site/www/js/main.js
+++ 2024knue/site/www/js/main.js
@@ -19,10 +19,11 @@
             $visualSlideNext = $('.visual .visual_wrap .visual_slide_wrap .next'),
             $visualSlideCurrent = $('.visual .visual_wrap .visual_slide_wrap .current'),
             $visualSlideTotal = $('.visual .visual_wrap .visual_slide_wrap .total');
+        //PC 전용
         $visualSlideList.slick({
             autoplay : true,
-            autoplaySpeed : 3000,
-            speed : 1000,
+            autoplaySpeed : 3900,
+            speed : 1300,
             dots : false,
             draggable : true,
             swipe : true,
@@ -43,6 +44,7 @@
             zIndex : 1,
             asNavFor : $MobilevisualSlideList
         });
+        //Mobile 전용
         $MobilevisualSlideList.slick({
             autoplay : true,
             autoplaySpeed : 3000,
@@ -159,7 +161,9 @@
             $cheongPrev = $('.cheong .cheong_wrap .prev'),
             $cheongNext = $('.cheong .cheong_wrap .next');
         $cheongSlide.slick({
-            autoplay : false,
+            autoplay : true,
+            autoplaySpeed : 3900,
+            speed : 1300,
             dots : false,
             draggable : true,
             swipe : true,
@@ -178,6 +182,131 @@
             zIndex : 1
         });
         //청람동정 슬라이드 끝
+
+        //학사일정 슬라이드 시작
+        var $scheSlide = $('.sche .sche_wrap .sche_slide_wrap .sche_slide_list'),
+            $schePrev = $('.sche .sche_wrap .sche_slide_wrap .prev'),
+            $scheNext = $('.sche .sche_wrap .sche_slide_wrap .next');
+        $scheSlide.slick({
+            autoplay : true,
+            autoplaySpeed : 3900,
+            speed : 1300,
+            dots : false,
+            draggable : true,
+            swipe : true,
+            swipeToSlide : true,
+            slidesToShow : 5,
+            slidesToScroll : 1,
+            variableWidth : false,
+            infinite : true,
+            arrows : true,
+            prevArrow : $schePrev,
+            nextArrow : $scheNext,
+            pauseOnArrowClick : true,
+            pauseOnDirectionKeyPush : true,
+            pauseOnSwipe : true,
+            pauseOnDotsClick : true,
+            zIndex : 1
+        });
+        //학사일정 슬라이드 끝
+
+        //팝업존 슬라이드 시작
+        var $popupSlide = $('.popup .popup_wrap .popup_slide_wrap .popup_slide_list');
+        $popupSlide.on('afterChange', function() {
+            setTimeout(function(){
+                $('.popup .popup_wrap .popup_slide_wrap .popup_slide_control .popup_nav *').removeAttr('role aria-controls aria-label aria-selected');
+            }, 1);
+        });
+        $popupSlide.slick({
+            autoplay : true,
+            autoplaySpeed : 3900,
+            dots : true,
+            appendDots : $('.popup .popup_wrap .popup_slide_wrap .popup_slide_control .popup_nav'),
+            dotsClass : 'popup_nav_list',
+            customPaging : function(slider, i) {
+                return '<button type="button"><span>'+(i + 1)+'번 보기</span></button>';
+            },
+            draggable : true,
+            swipe : true,
+            swipeToSlide : true,
+            slidesToShow : 1,
+            slidesToScroll : 1,
+            variableWidth : false,
+            infinite : true,
+            arrows : true,
+            autoArrow : $('.popup .popup_wrap .popup_slide_wrap .popup_slide_control .auto'),
+            pauseText : '정지',
+            playText : '재생',
+            pauseOnArrowClick : true,
+            pauseOnDirectionKeyPush : true,
+            pauseOnSwipe : true,
+            pauseOnDotsClick : true,
+            zIndex : 1,
+            fade : true
+        });
+        $('.popup .popup_wrap .popup_slide_wrap .popup_slide_control .popup_nav *').removeAttr('role aria-controls aria-label aria-selected');
+        //팝업존 슬라이드 끝
+
+        //청람 랜선투어 링크 효과 시작
+        var $lanLinkTitle = $('.lan .lan_link_title'),
+            $lanText = $lanLinkTitle.find('.text');
+        $lanText.each(function(){
+            var $this = $(this),
+                $thisItag = $this.find('i'),
+                $cloneItag = $thisItag.clone();
+            $this.append($cloneItag);
+        });
+        //청람 랜선투어 링크 효과 끝
+
+        //청람 SNS 슬라이드 탭 시작
+        var $snsBottomItem = $('.sns_bottom .sns_bottom_item');
+        $snsBottomItem.each(function(){
+            var $this = $(this),
+                $thisSlideList = $this.find('.sns_slide_list'),
+                $thisPrev = $this.find('.prev'),
+                $thisNext = $this.find('.next');
+            $thisSlideList.slick({
+                autoplay : true,
+                autoplaySpeed : 3900,
+                dots : false,
+                draggable : true,
+                swipe : true,
+                swipeToSlide : true,
+                slidesToShow : 3,
+                slidesToScroll : 1,
+                variableWidth : false,
+                infinite : true,
+                arrows : true,
+                prevArrow : $thisPrev,
+                nextArrow : $thisNext,
+                pauseOnArrowClick : true,
+                pauseOnDirectionKeyPush : true,
+                pauseOnSwipe : true,
+                pauseOnDotsClick : true,
+                zIndex : 1
+            });
+        });
+        $('.sns_tab .sns_tab_item button.sns_tab_btn').on('click', function(){
+            var $this = $(this),
+                $thisParent = $this.parent('.sns_tab_item'),
+                thisParentIndex = $thisParent.index(),
+                IsActive = $thisParent.is('.active'),
+                $otherParent = $thisParent.siblings('.sns_tab_item'),
+                $otherBtn = $otherParent.find('button.sns_tab_btn'),
+                $snsBottom = $('.sns_bottom'),
+                $thissnsBottomItem = $snsBottom.find('.sns_bottom_item').eq(thisParentIndex),
+                $othersnsBottomItem = $snsBottom.find('.sns_bottom_item').not($thissnsBottomItem);
+            if(!IsActive){
+                $otherParent.removeClass('active');
+                $otherBtn.removeAttr('title');
+                $othersnsBottomItem.removeClass('active');
+                $thisParent.addClass('active');
+                $this.attr('title', '선택됨');
+                $thissnsBottomItem.addClass('active');
+                $thissnsBottomItem.find('.sns_slide_wrap .sns_slide_list').slick('setPosition');
+            }
+        });
+        //청람 SNS 슬라이드 탭 끝
 
     });
 })(jQuery);
