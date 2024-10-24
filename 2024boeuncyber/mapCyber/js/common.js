61a3cb8b7c572c27458e669c8889ea27e461cfef
gunami4573 <bigunamict@naver.com>
2024-10-24 11:56:58 +0900

    2024 정이품보은군민 신규

---
+++ 2024boeuncyber/mapCyber/js/common.js
@@ -0,0 +1,84 @@
+//파라미터 추출 함수 시작
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+//파라미터 추출 함수 끝
+
+
+(function ($) {
+    'use strict';
+
+    var $window = $(window),
+        $document = $(document),
+        $html = $('html'),
+        $head = $('head'),
+        $screen = $.screen,
+        $inArray = $.inArray;
+
+    //브라우저
+    var _browser = navigator.userAgent.toLowerCase();
+    //ie7일 때
+    if (_browser.indexOf('msie 7.0') > -1) {
+        _browser = 'ie ie7';
+        //ie8일 때
+    } else if (_browser.indexOf('msie 8.0') > -1) {
+        _browser = 'ie ie8';
+        //ie9일 때
+    } else if (_browser.indexOf('msie 9.0') > -1) {
+        _browser = 'ie ie9';
+        //ie10일 때
+    } else if (_browser.indexOf('msie 10.0') > -1) {
+        _browser = 'ie ie10';
+        //ie11일 때
+    } else if (_browser.indexOf('trident/7.0') > -1) {
+        _browser = 'ie ie11';
+        //edge일 때
+    } else if (_browser.indexOf('edge') > -1) {
+        _browser = 'edge MS';
+    } else if (_browser.indexOf('edg/') > -1) {
+        _browser = 'edge chromium_based';
+        //opera일 때
+    } else if (_browser.indexOf('opr') > -1) {
+        _browser = 'opera';
+        //chrome일 때
+    } else if (_browser.indexOf('chrome') > -1) {
+        _browser = 'chrome';
+        //firefox일 때
+    } else if (_browser.indexOf('firefox') > -1) {
+        _browser = 'firefox';
+        //safari일 때
+    } else if (_browser.indexOf('safari') > -1) {
+        _browser = 'safari';
+    } else {
+        _browser = 'unknown';
+    }
+    window.getBrowser = function () {
+        return _browser;
+    };
+
+    //브라우저 클래스 추가
+    $html.addClass(_browser);
+
+    $(function () {
+        var $body = $('body'),
+            $htmlAndBody = $html.add($body),
+            $wrapper = $('#wrapper'),
+            $header = $('#header'),
+            $container = $('#container'),
+            $footer = $('#footer');
+
+        $window.on('screen:wide screen:web', function (event) {
+            window.mode = 'pc';
+        });
+        $window.on('screen:tablet screen:phone', function (event) {
+            window.mode = 'mobile';
+        });
+
+        //여기서부터 코드 작성해주세요
+
+    });
+
+})(jQuery);
